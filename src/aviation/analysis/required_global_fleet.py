8e5b06961cd4954fae29a25e4b8d400b04e5eb7b
Maja Glavin <meg64@cam.ac.uk>
2025-07-22 10:11:17 +0100

    Enable name-tests-test pre-commit hook

--- src/aviation/analysis/required_global_fleet.py
+++ src/aviation/analysis/required_global_fleet.py
@@ -5,7 +5,13 @@ seats_per_aircraft = 160.0
 flights_per_aircraft_per_day = 3.6
 days_per_year = 366.0
 
-passengers_per_day = aviation.passengers_per_day(passengers_per_year=passengers_per_year, days_per_year = days_per_year)
-global_fleet_size = aviation.required_global_fleet(passengers_per_day=passengers_per_day, seats_per_aircraft=seats_per_aircraft, flights_per_aircraft_per_day=flights_per_aircraft_per_day)
+passengers_per_day = aviation.passengers_per_day(
+    passengers_per_year=passengers_per_year, days_per_year=days_per_year
+)
+global_fleet_size = aviation.required_global_fleet(
+    passengers_per_day=passengers_per_day,
+    seats_per_aircraft=seats_per_aircraft,
+    flights_per_aircraft_per_day=flights_per_aircraft_per_day,
+)
 
 print(f"The global fleet size is: {global_fleet_size}")
---
+++ tests/test_fleet.py
@@ -0,0 +1,28 @@
+from aviation.fleet import passengers_per_day, required_global_fleet
+
+import pytest
+
+@pytest.mark.parametrize(
+    ("passengers_per_year", "days_per_year", "expected_passengers_per_day"),
+    (
+        (365_000_000.0, 365.0, 1_000_000.0),
+        (732_000_000.0, 366.0, 2_000_000.0),
+    ), 
+)
+def test_passengers_per_day(passengers_per_year, days_per_year, expected_passengers_per_day):
+    assert passengers_per_day(passengers_per_year, days_per_year) == expected_passengers_per_day
+
+def test_required_global_fleet():
+    days_per_year = 365.0
+    passengers_per_year = 5_000_000_000.0
+    seats_per_aircraft = 150.0
+    flights_per_aircraft_per_day = 2.0
+
+    expected_required_global_fleet = 25_000.0
+
+    result = required_global_fleet(
+        passengers_per_day(passengers_per_year, days_per_year),
+        seats_per_aircraft,
+        flights_per_aircraft_per_day
+    )
+    assert result == expected_required_global_fleet
