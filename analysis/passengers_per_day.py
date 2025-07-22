e6e7a70936cbc3c35e0b2248e2d3383567610409
Maja Glavin <meg64@cam.ac.uk>
2025-07-22 08:37:28 +0100

    edit message in

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -1,8 +1,8 @@
-import aviation
+import fleet
 
 passengers_per_year = 5_000_000_000.0
 days_per_year = 366.0
 
-passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
+passengers_per_day = fleet.passengers_per_day(passengers_per_year, days_per_year)
 
-print(f"{passengers_per_day=}")
+print(f"The number of passengers per day are: {passengers_per_day}")
