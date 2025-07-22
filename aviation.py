30e34300d499a41e7e0dba7c8b1fb2003d83ca9a
Maja Glavin <meg64@cam.ac.uk>
2025-07-22 08:20:06 +0100

    Refactor analysis into separate scripts

--- aviation.py
+++ aviation.py
@@ -1,11 +1,9 @@
 # Simple model 
 
-# Constants
-days_per_year = 365.0
-passengers_per_year = 5_000_000_000.0
-flights_per_aircraft = 2.5
-seats_per_aircraft = 180.0
+def passengers_per_day(passengers_per_year, days_per_year):
+    return passengers_per_year / days_per_year
 
-passengers_per_day = passengers_per_year / days_per_year
-required_global_fleet = passengers_per_day / (seats_per_aircraft * flights_per_aircraft)
-print(f"{required_global_fleet =:.2f}")
+def required_global_fleet(
+    passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day
+):
+    return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)
---
+++ required_global_fleet.py
@@ -0,0 +1,18 @@
+import aviation
+
+passengers_per_year = 5_000_000_000.0
+seats_per_aircraft = 160.0
+flights_per_aircraft_per_day = 3.6
+days_per_year = 366.0
+
+passengers_per_day = aviation.passengers_per_day(
+    passengers_per_year=passengers_per_year, days_per_year=days_per_year
+)
+
+required_global_fleet = aviation.required_global_fleet(
+    passengers_per_day=passengers_per_day,
+    seats_per_aircraft=seats_per_aircraft,
+    flights_per_aircraft_per_day=flights_per_aircraft_per_day,
+)
+
+print(f"{required_global_fleet=}")
